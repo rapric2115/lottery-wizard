98314a8e5b6b864e4d2c9a546f9584d25a540254
rapric2115 <raphael.richardson@gmail.com>
2025-02-07 14:38:02 -0400

    Upgrading expo sdk to 52

--- components/Resultados.js
+++ components/Resultados.js
@@ -356,7 +356,7 @@ const styles = StyleSheet.create({
       borderColor: '#247ba0',
       width: '49%'
     }, 
-    fetchResults: {     
+    fetchResults: { 
       margin: 4,
       textAlign: 'center',
       fontSize: 18,
